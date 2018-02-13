9f680229f2dae677df135cac99d5643f046bb2e8
Vini Tiet <tiet@mira-am.com>
2018-02-13 16:31:12 +0100

    beautiful wikivini

--- db/seeds.rb
+++ db/seeds.rb
@@ -5,3 +5,9 @@
 #
 #   movies = Movie.create([{ name: 'Star Wars' }, { name: 'Lord of the Rings' }])
 #   Character.create(name: 'Luke', movie: movies.first)
+
+10.times do
+  title = Faker::StarWars.quote
+  content = Faker::Lorem.paragraph(2, false, 4)
+  Article.create(title: title, content: content)
+end
